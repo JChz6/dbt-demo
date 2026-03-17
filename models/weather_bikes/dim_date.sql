f613f80d6dd2fb7924815eaf014592f8307b42b7
Jesús Chávez <jesuschavez612@gmail.com>
2026-03-17 05:08:47 +0000

    agregadas fuentes de 2018

--- models/weather_bikes/dim_date.sql
+++ models/weather_bikes/dim_date.sql
@@ -2,14 +2,21 @@ WITH CTE AS (
     SELECT
         started_at
     FROM {{ source('nyc_bikes', 'bike') }}
-    )
+    ),
+CTE_2018 AS(
     SELECT
-        TO_TIMESTAMP(started_at) AS start_ts,
-        DATE(TO_TIMESTAMP(started_at)) AS start_date,
-        TIME(TO_TIMESTAMP(started_at)) AS start_time,
-        HOUR(TO_TIMESTAMP(started_at)) AS start_hour,
-
-        {{businessday('started_at')}} AS businessday,
-        {{season_of_year('started_at')}} AS season_macro
-    
-    FROM CTE
+        TO_TIMESTAMP(REPLACE(starttime, '"', '')) AS started_at
+    FROM {{ source('nyc_bikes', 'bike_2018') }}
+),
+UNIFIED AS(
+    SELECT started_at FROM CTE
+        UNION ALL
+    SELECT started_at FROM CTE_2018
+)
+SELECT DISTINCT
+    DATE(TO_TIMESTAMP(started_at)) AS start_date,
+    DATE_TRUNC(MINUTE, TO_TIMESTAMP(started_at)) AS start_time,
+    HOUR(TO_TIMESTAMP(started_at)) AS start_hour,
+    {{businessday('started_at')}} AS businessday,
+    {{season_of_year('started_at')}} AS season_macro  
+FROM UNIFIED
--- models/weather_bikes/fact_bike_trips.sql
+++ models/weather_bikes/fact_bike_trips.sql
@@ -1,16 +1,39 @@
 WITH trips_cte AS(
     SELECT
-        *
+        ride_id,
+        DATE(TO_TIMESTAMP(started_at)) AS trip_date,
+        rideable_type,
+        TO_TIMESTAMP(started_at) AS started_at,
+        TO_TIMESTAMP(ended_at) AS ended_at,
+        TIMESTAMPDIFF(seconds, TO_TIMESTAMP(started_at), TO_TIMESTAMP(ended_at)) AS trip_duration,
+        start_station_id,
+        end_station_id,
+        LOWER(member_casual) AS usertype,
+        NULL AS user_birth_year,
+        NULL AS user_gender,
+        NULL AS bike_id
     FROM {{source('nyc_bikes', 'bike')}}
+),
+trips_cte_2018 AS(
+    SELECT
+        NULL AS ride_id,
+        DATE(TO_TIMESTAMP(REPLACE(starttime, '"', ''))) AS trip_date,
+        NULL AS rideable_type,
+        TO_TIMESTAMP(REPLACE(starttime, '"', '')) AS started_at,
+        TO_TIMESTAMP(REPLACE(stoptime, '"', '')) AS ended_at,
+        tripduration AS trip_duration,
+        start_station_id,
+        end_station_id,
+        LOWER(usertype) AS usertype,
+        birth_year AS user_birth_year,
+        gender AS user_gender,
+        bikeid AS bike_id
+    FROM {{source('nyc_bikes', 'bike_2018')}}
+),
+UNIFIED AS(
+    SELECT * FROM trips_cte
+    UNION ALL
+    SELECT * FROM trips_cte_2018
 )
-SELECT
-    ride_id,
-    DATE(TO_TIMESTAMP(started_at)) AS trip_date,
-    rideable_type,
-    TO_TIMESTAMP(started_at) AS started_at,
-    TO_TIMESTAMP(ended_at) AS ended_at,
-    ROUND(TIMESTAMPDIFF(seconds, TO_TIMESTAMP(started_at), TO_TIMESTAMP(ended_at))/60, 2) AS trip_duration_min,
-    start_station_id,
-    end_station_id,
-    member_casual
-FROM trips_cte
+SELECT * FROM UNIFIED
+
